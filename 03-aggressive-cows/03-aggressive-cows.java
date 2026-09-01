b2541513b0e6982dc265886e87c2b6274973e2af
Yashraj Prajapati <yashrajprajapati4002@gmail.com>
2026-09-01 23:05:12 +0530

    Implement aggressive cows placement algorithm

--- 03-aggressive-cows/03-aggressive-cows.java
+++ 03-aggressive-cows/03-aggressive-cows.java
@@ -1,1 +1,56 @@
+import java.util.*;
 
+class Solution {
+
+    public int aggressiveCows(int[] stalls, int k) {
+
+        Arrays.sort(stalls);
+
+        int start = 1;
+        int end = stalls[stalls.length - 1] - stalls[0];
+
+        int answer = 0;
+
+        while (start <= end) {
+
+            int mid = (start + end) / 2;
+
+            if (canPlace(stalls, k, mid)) {
+
+                // mid distance possible hai
+                answer = mid;
+
+                // aur bada distance try karo
+                start = mid + 1;
+
+            } else {
+
+                // mid distance possible nahi hai
+                end = mid - 1;
+            }
+        }
+
+        return answer;
+    }
+
+    public boolean canPlace(int[] stalls, int k, int distance) {
+
+        int cows = 1;
+        int lastPosition = stalls[0];
+
+        for (int i = 1; i < stalls.length; i++) {
+
+            if (stalls[i] - lastPosition >= distance) {
+
+                cows++;
+                lastPosition = stalls[i];
+            }
+
+            if (cows >= k) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
