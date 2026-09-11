f79b24ac2e464cf1027fe2993268bade445208fd
Yashraj Prajapati <yashrajprajapati4002@gmail.com>
2026-09-11 22:47:53 +0530

    Update 06-reverse-first-K-of-a-queue.java

--- 06-reverse-first-K-of-a-queue/06-reverse-first-K-of-a-queue.java
+++ 06-reverse-first-K-of-a-queue/06-reverse-first-K-of-a-queue.java
@@ -1,1 +1,31 @@
-
+class Solution {
+    public Queue<Integer> reverseFirstK(Queue<Integer> q, int k) {
+        // code here
+        
+        if(k > q.size())
+        {
+            return q;
+        }
+        
+        Stack<Integer> s = new Stack<>();
+        
+        for(int i = 1; i<=k; i++)
+        {
+            s.push(q.remove());
+        }
+        
+        while(!s.isEmpty())
+        {
+            q.add(s.pop());
+        }
+        
+        int remain = q.size() - k;
+        
+        for(int i = 0; i<remain; i++)
+        {
+            q.add(q.remove());
+        }
+        
+        return q;
+    }
+}
